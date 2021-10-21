4938c8b8aa7054b381c1cfedf7e2b414956202af
Cha Hoseong <hoseong@chahoseong.com>
2021-10-21 17:29:13 +0900

    Sequence Animation - show sequence elements

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ searchInputElement.addEventListener('blur', function () {
 const badgeElement = document.querySelector('header .badges');
 window.addEventListener('scroll', _.throttle(function () {
     if (window.scrollY > 500) {
+        // gsap.to(요소, 지속시간, 속성)
         gsap.to(badgeElement, 0.6, {
             opacity: 0,
             display: 'none'
@@ -29,3 +30,11 @@ window.addEventListener('scroll', _.throttle(function () {
         });
     }
 }, 300));
+
+const fadeElements = document.querySelectorAll('.visual .fade-in');
+fadeElements.forEach(function (fadeElement, index) {
+    gsap.to(fadeElement, 1.0, {
+        delay: 0.7 * (index + 1),
+        opacity: 1
+    });
+});
